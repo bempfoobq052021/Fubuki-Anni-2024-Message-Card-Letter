8ef5031f945b64afd8575330c989b1e2b924efdb
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2024-04-11 20:38:10 +0700

    Beginning Testing for Knob
    Created knob testing, deleting some css and editing some stuff.

--- LETTER PROJECT TESTING ZONE/CARD_Animation_Testing/index.js
+++
@@ -1,4 +0,0 @@
-async function mainAnimation(){
-	
-	return 0;
-}
--- LETTER PROJECT TESTING ZONE/Coin - Transparency Experiment/script.js
+++ LETTER PROJECT TESTING ZONE/Coin - Transparency Experiment/script.js
@@ -1,43 +1,3 @@
-/* // common function to apply one transition rule
-function transition(elem, styleProps) {
-  return new Promise((resolve, reject) => {
-    function handleTransitionEnd() {
-      console.log("Transition Ended...");
-      resolve(elem);
-    }
-    elem.addEventListener("transitionend", handleTransitionEnd, { once: true });
-    Object.entries(styleProps).forEach(([prop, value]) => {
-      elem.style.setProperty(prop, value);
-    });
-  });
-}
-
-// common function to apply animations to an element.
-function animate(elem, animation, remove_animation = null) {
-  return new Promise((resolve, reject) => {
-    function handleAnimationEnd() {
-      console.log("animation ended...");
-      resolve(elem);
-    }
-    elem.addEventListener("animationend", handleAnimationEnd, { once: true });
-    if(remove_animation != null) elem.classList.remove(remove_animation);
-    elem.classList.add(animation);
-  });
-}
-
-async function init() {
-  const LETTER = document.querySelector(".LETTER");
-  // first fadeout text
-  await animate(LETTER, "move_down");
-  console.log("moving down");
-  await animate(LETTER, "flip_and_return", "move_down");
-  console.log("flip and return");
-}
-
-document.addEventListener("DOMContentLoaded", (e) => {
-  document.querySelector("#drawcard").addEventListener("click", init);
-}); */
-
 /* Dragging Behaviour and Bounding Box Checks for Coins 
 Source: https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop2
 */
@@ -49,7 +9,7 @@ function coin_init(ev){
 }
 
 function coin_enters_target(ev){
-    ev.target.classList.add("shining");
+    
     document.getElementById("debug_coin_state_display").innerHTML = "entering target";
 }
 
@@ -60,14 +20,15 @@ function coin_hovering(ev){
 function coin_drop(ev){
     ev.preventDefault();
     var data = ev.dataTransfer.getData("element_class");
-    document.getElementById("debug_coin_state_display").innerHTML = ev.target.className;
-    if(ev.target.className == "drop_target"){
+    if(ev.target.id !== "drop_target"){
         document.getElementById("debug_coin_state_display").innerHTML = "dropped outside target";
+        document.getElementById("ev_target_id").innerHTML = ev.target.id;
         return 10;
     }
     else {
-        ev.target.appendChild(document.getElementById(data));
         document.getElementById("debug_coin_state_display").innerHTML = "coin dropped";
+        ev.target.appendChild(document.getElementById(data));
+        document.getElementById("ev_target_id").innerHTML = ev.target.id;
         console.log("coin dropped");
     }
 }
@@ -77,5 +38,5 @@ function coin_position_readjust(ev){
 }
 
 function coin_leaves_target(ev){
-    ev.target.classList.remove("shining");
+    
 }
